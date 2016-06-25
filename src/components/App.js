4c7a08a13da6007824ebc682eb17eab753a7bbc1
justinhuynh <jhuynh@gmail.com>
2016-06-25 16:49:53 -0400

    updated initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-let groceries = [
-  { id: 1, name: "Oranges" },
-  { id: 2, name: "Bananas" },
-  { id: 3, name: "Bread" }
-];
-
 const App = props => {
-  let groceryList = groceries.map((grocery) => {
+  let groceryList = props.groceries.map((grocery) => {
     return (
       <li key={grocery.id}>{grocery.name}</li>
     );
--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+let groceries = [
+  { id: 1, name: "Oranges" },
+  { id: 2, name: "Bananas" },
+  { id: 3, name: "Bread" }
+];
+
 ReactDOM.render(
-  <App />,
+  <App groceries={groceries} />,
   document.getElementById('app')
 );
