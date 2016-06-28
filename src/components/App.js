8ee41a1232d7588075e24f97707f2b626349b333
justinhuynh <jhuynh@gmail.com>
2016-06-28 06:51:23 -0400

    downgrade App to const

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import GroceryList from './GroceryList';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Grocery List!</h1>
-        <GroceryList
-          groceries={this.props.groceries} />
-      </div>
-    )
-  }
+const App = props => {
+  return (
+    <div>
+      <h1>Grocery List!</h1>
+      <GroceryList
+        groceries={props.groceries} />
+    </div>
+  )
 }
 
 export default App;
