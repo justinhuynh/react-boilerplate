fdf227b8fd1628b9c4328b18a98fbe597c18cfa7
Launch Academy <launchacademy@PresentationMachine.local>
2016-06-28 11:49:51 -0400

    blank

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 
 const App = props => {
-  let groceryList = props.groceries.map((grocery) => {
-    return (
-      <li key={grocery.id}>{grocery.name}</li>
-    );
-  })
-
   return (
     <div>
-      {groceryList}
     </div>
   )
 }
--- src/main.js
+++ src/main.js
@@ -4,13 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-let groceries = [
-  { id: 1, name: "Oranges" },
-  { id: 2, name: "Bananas" },
-  { id: 3, name: "Bread" }
-];
-
 ReactDOM.render(
-  <App groceries={groceries} />,
+  <App />,
   document.getElementById('app')
 );
